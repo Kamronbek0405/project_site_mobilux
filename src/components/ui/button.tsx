882234fe9c5e7f7d16941643eabf09f81bad0476
Kamran0504 <kamrannutbook05@gmail.com>
2024-09-20 14:54:53 +0500

    project

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        basket: " w-[250px] ml-2 mt-2 hover:bg-slate-100  hover:text-black bg-green-600 text-white",
+        basket: "w-[250px] ml-2 mt-2 hover:bg-slate-100 hover:text-black bg-green-600 text-white",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -38,17 +38,24 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  startIcon?: React.ReactNode
+  endIcon?: React.ReactNode
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, startIcon, endIcon, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {startIcon && <span className="mr-2">{startIcon}</span>}
+        {children}
+        {endIcon && <span className="ml-2">{endIcon}</span>}
+      </Comp>
     )
   }
 )
